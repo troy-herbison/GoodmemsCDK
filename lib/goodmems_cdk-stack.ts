78e22d7e9e0b1115e4fe3112309e485a4b08cbf1
Troy Herbison <troy.herbison@gmail.com>
2021-04-20 09:32:53 -0500

    Working template for backend

--- lib/goodmems_cdk-stack.ts
+++ lib/goodmems_cdk-stack.ts
@@ -1,9 +1,36 @@
-import * as cdk from '@aws-cdk/core';
+import * as core from "@aws-cdk/core";
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as s3 from "@aws-cdk/aws-s3";
 
-export class GoodmemsCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class GoodmemsCdkStack extends core.Stack {
+  constructor(scope: core.App, id: string) {
+    super(scope, id);
 
     // The code that defines your stack goes here
+
+    const bucket = new s3.Bucket(this, "WidgetStore");
+
+    const handler = new lambda.Function(this, "WidgetHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
+      code: lambda.Code.fromAsset("resources"),
+      handler: "widgets.main",
+      environment: {
+        BUCKET: bucket.bucketName
+      }
+    });
+
+    bucket.grantReadWrite(handler); // was: handler.role);
+
+    const api = new apigateway.RestApi(this, "widgets-api", {
+      restApiName: "Widget Service",
+      description: "This service serves widgets."
+    });
+
+    const getWidgetsIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("GET", getWidgetsIntegration); // GET /
   }
 }
